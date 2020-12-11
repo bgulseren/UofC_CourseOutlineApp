fe736173b81f47e04518cac209201a7e39999364
KBehairy <kbehairy@ucalgary.ca>
2020-12-11 04:55:06 +0000

    Implemented counter application

--- REACT/reactapp/src/App.js
+++ REACT/reactapp/src/App.js
@@ -1,6 +1,18 @@
 import "bulma/css/bulma.css";
-
+import{ useState } from "react";
 function App() {
+
+  // initializing the counter with 0
+  const [counter, setCounter] = useState(0); 
+  const [inputValue, setInputValue] = useState("");
+
+  // for setting the counter using input value when pressing enter key
+  const handleKeyDown=(e)=>{
+    if(e.key=="Enter"){
+      setCounter(parseInt(inputValue))
+    }
+  };
+
   return (
     <div className="App">
       <div className="container">
@@ -15,19 +27,22 @@ function App() {
                         className="input"
                         type="text"
                         placeholder="Enter a number"
+                        value={inputValue}
+                        onChange={(e)=>setInputValue(e.target.value)}
+                        onKeyDown={handleKeyDown}
                       />
                     </div>
                     <div className="control">
-                      <a className="button is-info">Assign</a>
+                      <a className="button is-info" onClick={()=>setCounter(parseInt(inputValue))}>Assign</a>
                     </div>
                   </div>
                   <div className="buttons has-addons">
-                    <button className="button is-primary">Increase</button>
-                    <button className="button is-warning">Decrease</button>
+                    <button className="button is-primary" onClick={()=>setCounter(counter+1)}>Increase</button>
+                    <button className="button is-warning"onClick={()=>setCounter(counter-1)}>Decrease</button>
                   </div>
                 </div>
                 <div className="column is-half">
-                  <h1 className="title">value</h1>
+                  <h1 className="title">{counter}</h1>
                 </div>
               </div>
             </div>
