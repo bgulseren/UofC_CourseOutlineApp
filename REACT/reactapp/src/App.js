1b5a07be08887f06dd517d59c9925047e5dbe049
KBehairy <kbehairy@ucalgary.ca>
2020-12-11 04:15:43 +0000

    Creating app UI

--- REACT/reactapp/src/App.js
+++ REACT/reactapp/src/App.js
@@ -1,7 +1,39 @@
+import "bulma/css/bulma.css";
+
 function App() {
   return (
     <div className="App">
-      
+      <div className="container">
+        <div className="columns is-multiline">
+          <div className="column is-full">
+            <div className="notification">
+              <div className="columns">
+                <div className="column is-half">
+                  <div className="field has-addons">
+                    <div className="control">
+                      <input 
+                        className="input"
+                        type="text"
+                        placeholder="Enter a number"
+                      />
+                    </div>
+                    <div className="control">
+                      <a className="button is-info">Assign</a>
+                    </div>
+                  </div>
+                  <div className="buttons has-addons">
+                    <button className="button is-primary">Increase</button>
+                    <button className="button is-warning">Decrease</button>
+                  </div>
+                </div>
+                <div className="column is-half">
+                  <h1 className="title">value</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
