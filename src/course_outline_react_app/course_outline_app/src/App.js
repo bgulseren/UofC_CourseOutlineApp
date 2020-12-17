fed5150023fc646c8e2466d30ab3bba72c776714
KBehairy <kbehairy@ucalgary.ca>
2020-12-17 09:34:55 +0000

    implemented structure of UI
    TODO: implement functionality

--- src/course_outline_react_app/course_outline_app/src/App.js
+++ src/course_outline_react_app/course_outline_app/src/App.js
@@ -1,8 +1,97 @@
 import "bulma/css/bulma.css";
 import React from 'react';
 
-function App() {
+const LearningOutcomeTableComponent = () => {
+  return (
+    <table className="table is-striped is-hoverable" id="learningOutcomesTable">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Learning Outcome</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>1</th>
+          <td>Have a deep understanding, and practical knowledge of object oriented analysis, design, and development.</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
 
+const GradeBreakdownTableComponent = () => {
+  return (
+    <table className="table is-striped is-hoverable" id="gradeBreakdownTable">
+      <thead>
+        <tr>
+          <th>Grading Component</th>
+          <th>Learning Outcome(s) Evaluated</th>
+          <th>Weight</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>Assignments</th>
+          <td>1,2,5</td>
+          <td>80%</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+
+const LearningOutcomesComponent = () => {
+  return (
+    <div>
+      <h1 className="title">Learning Outcomes</h1>
+      <LearningOutcomeTableComponent />      
+      <div className="buttons has-addons">
+        <button className="button is-primary" onClick={()=>document.getElementById('learningOutcomesTable').getElementsByTagName('tbody')[0].insertRow(0)}>Add learning Outcome</button>
+        <button className="button is-danger" onClick={()=>document.getElementById('learningOutcomesTable').getElementsByTagName('tbody')[0].deleteRow(0)}>Delete Learning Outcome</button>
+      </div>
+    </div>
+  );
+};
+
+
+const AddGradeComponentPopup = () => {
+  return (
+    <div className="modal" id="addGradeModal">
+      <div className="modal-background"></div>
+      <div className="modal-content">
+        <h1 className="title">TEST</h1>
+      </div>
+      <button className="modal-close is-large" aria-label="close"></button>
+    </div>
+  );
+}
+
+
+const GradeBreakdownComponent = () => {
+  function addButtonClickHandler(){
+    var modal = document.getElementById("addGradeModal");
+    document.getElementById('learningOutcomesTable').getElementsByTagName('tbody')[0].insertRow()
+  }
+  function deleteButtonClickHandler(){
+    document.getElementById('learningOutcomesTable').getElementsByTagName('tbody')[0].deleteRow(0)
+  }
+  return (
+    <div>
+      <h1 className="title">Final Grade Breakdown</h1>
+      <GradeBreakdownTableComponent />      
+      <div className="buttons has-addons">
+        <button className="button is-primary" onClick={addButtonClickHandler}>Add Grade Component</button>
+        <button className="button is-danger" onClick={deleteButtonClickHandler}>Delete Grade Component</button>
+      </div>
+      <textarea className="textarea" readOnly>This content is readonly</textarea>
+    </div>
+  );
+};
+
+
+function App() {
   return (
     <div>
     <h1 className="title">Course Outline Generator</h1>
@@ -16,23 +105,13 @@ function App() {
 
       <section className="learning outcomes section">
         <div className="container">
-          <h1 className="title">Learning Outcomes</h1>
-            <textarea className="textarea" placeholder="Enter Text"></textarea>
-              <div className="buttons has-addons">
-                <button className="button is-primary">Add learning Outcome</button>
-                <button className="button is-danger">Delete Learning Outcome</button>
-            </div>
+          <LearningOutcomesComponent />  
         </div>
       </section>
 
       <section className="grade breakdown section">
         <div className="container">
-          <h1 className="title">Final Grade Breakdown</h1>
-            <textarea className="textarea" placeholder="Enter Text"></textarea>
-              <div className="buttons has-addons">
-                <button className="button is-primary">Add Grade Component</button>
-                <button className="button is-danger">Delete Grade Component</button>
-              </div>
+          <GradeBreakdownComponent />
         </div>
       </section>
 
