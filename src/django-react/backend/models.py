3f6bd29f285238f387875a64a5e4de48e2529a5e
KBehairy <kbehairy@ucalgary.ca>
2021-01-05 17:10:23 -0700

    created django models for outline, learning outcomes, grading brakedown

--- src/django-react/backend/models.py
+++ src/django-react/backend/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Course outline model
+class outline(models.Model):
+    text = models.CharField(max_length = 300)
+
+# Learning outcomes model
+class learningOutcomes(models.Model):
+    text = models.CharField(max_length = 100)
+
+# Final grade breakdown model
+class gradeBreakdown(models.Model):
+    gradingComponent = models.CharField(max_lenght = 50)
+    learningOutcomes = models.CharField(max_lenght = 20)
+    weight = models.IntegerField()
