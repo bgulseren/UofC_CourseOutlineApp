93f365feef9816aec356a17dffc7c7de0e115b54
Burak Gulseren <gulserenburak@gmail.com>
2021-01-20 19:41:17 -0700

    Clean-up.

--- src/django-react/django_backend/backend/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/create-outline/', views.CreateOutlineView.as_view()),
-    path('api/create-learningoutcome/', views.CreateLearningOutcomesView.as_view()),
-]
