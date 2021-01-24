a4c55a8225a4e6d9bf4d3bd23828a1ada3b76e16
Burak Gulseren <gulserenburak@gmail.com>
2021-01-24 13:23:13 -0700

    Added logo and header.

--- src/course_outline_app/src/App.js
+++ src/course_outline_app/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+import { ReactComponent as Logo } from './UCalgary.svg';
 import Instructor from './components/Instructor';
+import "./components/App.css";
 
 function App() {
 
   return (
     <React.Fragment>
-      <Instructor />
+      <CssBaseline />
+        <Container fixed >
+          <div container className="App-header" spacing={1}>
+              <Logo className="logo"/>
+              <h3>Course Outline Application</h3>
+          </div>
+
+          <Typography component="div" style={{height: '100vh' }}>
+
+            <Instructor />
+          </Typography>
+        </Container>
     </React.Fragment>
   );
 }
