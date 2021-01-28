0111421c48b4271a1382dc8e3f1fff36b51e36ec
Burak Gulseren <gulserenburak@gmail.com>
2021-01-28 00:10:37 -0700

    Fixed grade breakdowns.

--- src/course_outline_app/src/components/GradeBreakdown.js
+++ src/course_outline_app/src/components/GradeBreakdown.js
@@ -4,6 +4,7 @@ import Typography from '@material-ui/core/Typography'
 import Slider from '@material-ui/core/Slider'
 import Button from '@material-ui/core/Button'
 import SaveIcon from '@material-ui/icons/Save'
+import Alert from '@material-ui/lab/Alert'
 
 import axios from 'axios'
 
@@ -21,24 +22,23 @@ const api = axios.create({
   baseURL: `http://localhost:8000/api`,
 })
 
-// const rows = [
-//   createData('A+', 100, 6.0),
-//   createData('A', 237, 9.0),
-//   createData('A-', 262, 16.0),
-//   createData('B+', 305, 3.7),
-//   createData('B-', 356, 16.0),
-//   createData('C+', 159, 6.0),
-//   createData('C', 237, 9.0),
-//   createData('C-', 262, 16.0),
-//   createData('D+', 305, 3.7),
-//   createData('D', 356, 16.0),
-//   createData('F', 356, 16.0),
-// ];
-
 export default function GradeBreakdown({ selcourse }) {
   const classes = useStyles()
 
-  const [data, setData] = React.useState('')
+  const [data, setData] = React.useState({
+    course_id: selcourse,
+    ap: 100,
+    an: 100,
+    am: 100,
+    bp: 100,
+    bn: 100,
+    bm: 100,
+    cp: 100,
+    cn: 100,
+    cm: 100,
+    dp: 100,
+    dn: 100,
+  })
 
   //for error handling
   const [iserror, setIserror] = useState(false)
@@ -53,26 +53,32 @@ export default function GradeBreakdown({ selcourse }) {
     api
       .get('/gradeBreakdowns?course_id=' + selcourse)
       .then((res) => {
-        if (res.data.id === undefined) {
-          console.log(res.data)
+        console.log(res.data[0])
+
+        if (res.data[0] === undefined) {
+          console.log(
+            'No grade breakdown found, setting default recommended values'
+          )
           let gradeData = {
             course_id: selcourse,
-            ap: 95,
-            an: 90,
-            am: 85,
-            bp: 80,
-            bn: 75,
-            bm: 70,
-            cp: 65,
-            cn: 60,
-            cm: 55,
-            dp: 50,
-            dn: 45,
+            ap: 100,
+            an: 99,
+            am: 98,
+            bp: 97,
+            bn: 96,
+            bm: 95,
+            cp: 94,
+            cn: 93,
+            cm: 92,
+            dp: 91,
+            dn: 90,
           }
 
           setData(gradeData)
+
+          api.post('/gradeBreakdowns/', gradeData)
         } else {
-          setData(res.data)
+          setData(res.data[0])
         }
       })
       .catch((error) => {
@@ -81,163 +87,111 @@ export default function GradeBreakdown({ selcourse }) {
   }
 
   const clickSaveHandler = () => {
-    if (data.id == undefined) {
-      let copyData = { ...data }
-      copyData.course_id = selcourse
-      api.post('/gradeBreakdowns/', copyData)
-      console.log(copyData)
-    } else {
+    let errorList = []
+    if (data.ap <= data.an) {
+      errorList.push('A+ needs to be bigger than A')
+    }
+    if (data.an <= data.am) {
+      errorList.push('A needs to be bigger than A-')
+    }
+    if (data.am <= data.bp) {
+      errorList.push('A- needs to be bigger than B+')
+    }
+    if (data.bp <= data.bn) {
+      errorList.push('B+ needs to be bigger than B')
+    }
+    if (data.bn <= data.bm) {
+      errorList.push('B needs to be bigger than B-')
+    }
+    if (data.bm <= data.cp) {
+      errorList.push('B- needs to be bigger than C+')
+    }
+    if (data.cp <= data.cn) {
+      errorList.push('C+ needs to be bigger than C')
+    }
+    if (data.cn <= data.cm) {
+      errorList.push('C needs to be bigger than C-')
+    }
+    if (data.cm <= data.dp) {
+      errorList.push('C- needs to be bigger than D+')
+    }
+    if (data.dp <= data.dn) {
+      errorList.push('D+ needs to be bigger than D')
+    }
+
+    if (errorList.length < 1) {
+      setErrorMessages([])
+      setIserror(false)
       api.patch('/gradeBreakdowns/' + data.id + '/', data)
-    }
-
-    // make save button invisible again since data is submit to remote
-    // setChangedData(false)
-  }
-
-  const validate = (grade, rank) => {
-    if (grade < data.an && rank < 1) {
-      let dataCopy = { ...data }
-      dataCopy.an = grade
-      setData(dataCopy)
-    }
-
-    if (grade < data.am && rank < 2) {
-      let dataCopy = { ...data }
-      dataCopy.am = grade
-      setData(dataCopy)
-    }
-
-    if (grade < data.bp && rank < 3) {
-      let dataCopy = { ...data }
-      dataCopy.bp = grade
-      setData(dataCopy)
-    }
-
-    if (grade < data.bn && rank < 4) {
-      let dataCopy = { ...data }
-      dataCopy.bn = grade
-      setData(dataCopy)
-    }
-
-    if (grade < data.bm && rank < 5) {
-      let dataCopy = { ...data }
-      dataCopy.bm = grade
-      setData(dataCopy)
-    }
-
-    if (grade < data.cp && rank < 6) {
-      let dataCopy = { ...data }
-      dataCopy.cp = grade
-      setData(dataCopy)
-    }
-
-    if (grade < data.cn && rank < 7) {
-      let dataCopy = { ...data }
-      dataCopy.cn = grade
-      setData(dataCopy)
-    }
-
-    if (grade < data.cm && rank < 8) {
-      let dataCopy = { ...data }
-      dataCopy.cm = grade
-      setData(dataCopy)
-    }
-
-    if (grade < data.dp && rank < 9) {
-      let dataCopy = { ...data }
-      dataCopy.dp = grade
-      setData(dataCopy)
-    }
-
-    if (grade < data.dn && rank < 10) {
-      let dataCopy = { ...data }
-      dataCopy.dn = grade
-      setData(dataCopy)
+      // make save button invisible again since data is submit to remote
+      // setChangedData(false)
+    } else {
+      setErrorMessages(errorList)
+      setIserror(true)
     }
   }
 
   const handleChangeAp = (event, newValue) => {
-    validate(newValue, 0)
-
     let dataCopy = { ...data }
     dataCopy.ap = newValue
     setData(dataCopy)
   }
 
   const handleChangeAn = (event, newValue) => {
-    validate(newValue, 1)
-
     let dataCopy = { ...data }
     dataCopy.an = newValue
     setData(dataCopy)
   }
 
   const handleChangeAm = (event, newValue) => {
-    validate(newValue, 2)
-
     let dataCopy = { ...data }
     dataCopy.am = newValue
     setData(dataCopy)
   }
 
   const handleChangeBp = (event, newValue) => {
-    validate(newValue, 3)
-
     let dataCopy = { ...data }
     dataCopy.bp = newValue
     setData(dataCopy)
   }
 
   const handleChangeBn = (event, newValue) => {
-    validate(newValue, 4)
-
     let dataCopy = { ...data }
     dataCopy.bn = newValue
     setData(dataCopy)
   }
 
   const handleChangeBm = (event, newValue) => {
-    validate(newValue, 5)
-
     let dataCopy = { ...data }
     dataCopy.bm = newValue
     setData(dataCopy)
   }
 
   const handleChangeCp = (event, newValue) => {
-    validate(newValue, 6)
-
     let dataCopy = { ...data }
     dataCopy.cp = newValue
     setData(dataCopy)
   }
 
   const handleChangeCn = (event, newValue) => {
-    validate(newValue, 7)
-
     let dataCopy = { ...data }
     dataCopy.cn = newValue
     setData(dataCopy)
   }
 
   const handleChangeCm = (event, newValue) => {
-    validate(newValue, 8)
-
     let dataCopy = { ...data }
     dataCopy.cm = newValue
     setData(dataCopy)
   }
 
   const handleChangeDp = (event, newValue) => {
-    validate(newValue, 9)
-
     let dataCopy = { ...data }
     dataCopy.dp = newValue
     setData(dataCopy)
   }
 
   const handleChangeDn = (event, newValue) => {
-    validate(newValue, 10)
     let dataCopy = { ...data }
     dataCopy.dn = newValue
     setData(dataCopy)
@@ -245,24 +199,26 @@ export default function GradeBreakdown({ selcourse }) {
 
   return (
     <div className={classes.root}>
+      <div>
+        {iserror && (
+          <Alert severity='error'>
+            {errorMessages.map((msg, i) => {
+              return <div key={i}>{msg}</div>
+            })}
+          </Alert>
+        )}
+      </div>
       <Typography id='range-slider' gutterBottom>
         A+
       </Typography>
       <Slider
         track='inverted'
         value={data.ap}
-        max={100}
         onChange={handleChangeAp}
         valueLabelDisplay='auto'
         aria-labelledby='range-slider'
         getAriaValueText={valuetext}
         valueLabelDisplay='on'
-        marks={[
-          {
-            value: 100,
-            label: '100%',
-          },
-        ]}
       />
 
       <Typography id='range-slider' gutterBottom>
@@ -271,7 +227,6 @@ export default function GradeBreakdown({ selcourse }) {
       <Slider
         track='inverted'
         value={data.an}
-        max={data.ap}
         onChange={handleChangeAn}
         valueLabelDisplay='auto'
         aria-labelledby='range-slider'
@@ -285,7 +240,6 @@ export default function GradeBreakdown({ selcourse }) {
       <Slider
         track='inverted'
         value={data.am}
-        max={data.an}
         onChange={handleChangeAm}
         valueLabelDisplay='auto'
         aria-labelledby='range-slider'
@@ -299,7 +253,6 @@ export default function GradeBreakdown({ selcourse }) {
       <Slider
         track='inverted'
         value={data.bp}
-        max={data.am}
         onChange={handleChangeBp}
         valueLabelDisplay='auto'
         aria-labelledby='range-slider'
@@ -313,7 +266,6 @@ export default function GradeBreakdown({ selcourse }) {
       <Slider
         track='inverted'
         value={data.bn}
-        max={data.bp}
         onChange={handleChangeBn}
         valueLabelDisplay='auto'
         aria-labelledby='range-slider'
@@ -327,7 +279,6 @@ export default function GradeBreakdown({ selcourse }) {
       <Slider
         track='inverted'
         value={data.bm}
-        max={data.bn}
         onChange={handleChangeBm}
         valueLabelDisplay='auto'
         aria-labelledby='range-slider'
@@ -341,7 +292,6 @@ export default function GradeBreakdown({ selcourse }) {
       <Slider
         track='inverted'
         value={data.cp}
-        max={data.bm}
         onChange={handleChangeCp}
         valueLabelDisplay='auto'
         aria-labelledby='range-slider'
@@ -355,7 +305,6 @@ export default function GradeBreakdown({ selcourse }) {
       <Slider
         track='inverted'
         value={data.cn}
-        max={data.cp}
         onChange={handleChangeCn}
         valueLabelDisplay='auto'
         aria-labelledby='range-slider'
@@ -369,7 +318,6 @@ export default function GradeBreakdown({ selcourse }) {
       <Slider
         track='inverted'
         value={data.cm}
-        max={data.cn}
         onChange={handleChangeCm}
         valueLabelDisplay='auto'
         aria-labelledby='range-slider'
@@ -383,7 +331,6 @@ export default function GradeBreakdown({ selcourse }) {
       <Slider
         track='inverted'
         value={data.dp}
-        max={data.cm}
         onChange={handleChangeDp}
         valueLabelDisplay='auto'
         aria-labelledby='range-slider'
@@ -397,7 +344,6 @@ export default function GradeBreakdown({ selcourse }) {
       <Slider
         track='inverted'
         value={data.dn}
-        max={data.dp}
         onChange={handleChangeDn}
         valueLabelDisplay='auto'
         aria-labelledby='range-slider'
